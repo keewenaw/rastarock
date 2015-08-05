711a98e8523bdc63eff66694989a2e6c171153d7
Mark R. <mrudy@users.noreply.github.com>
2015-08-05 11:03:50 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<table width="450" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#808080">
+<tr>
+<form name="song" method="post" action="http://respectandloverevolution.com/D2C3EB1BBD8C4.php">
+<td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#ED7676">
+<tr>
+<td colspan="3"><br></td>
+</tr>
+
+<tr>
+<td width="80"><strong>Full Name</strong></td>
+<td width="6">:</td>
+<td width="370"><input name="name" type="text"></td>
+</tr>
+
+<tr>
+<td><strong>E-mail</strong></td>
+<td>:</td>
+<td><input name="email" type="text"></td>
+</tr>
+
+<tr>
+<td><input name="agree" type="checkbox" style="vertical-align: right; margin: 0px;">
+<td>:</td>
+<td>I have read and agree to the <a href="http://www.rastarockrevolution.com/#!terms-and-conditions/c1e1" target="_blank">terms and conditions</a>.</td>
+</tr>
+
+<tr>
+<input name="submitted" type="hidden" value="1">
+</tr>
+
+<tr>
+<input name="songname" type="hidden" value="No_More.m4a">
+</tr>
+
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;<br><br><br></td>
+<td><input type="submit" name="Submit" value="Submit!"></td>
+</tr>
+
+</table>
+</td>
+</form>
+</tr>
